547d582e45537e5b28ac4bce460531b118ee57bc
youssefbassem1 <youssefbassem12@gmail.com>
2025-03-01 18:43:24 +0200

    Update Cart.java

--- src/main/java/com/example/model/Cart.java
+++ src/main/java/com/example/model/Cart.java
@@ -1,11 +1,13 @@
 package com.example.model;
 
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-
+@Component
 public class Cart {
     private UUID id;
     private UUID userId;
