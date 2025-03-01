7aa18c8c4dbb554fb4c3e38ea55fb7f979ffd163
George Sherif <georgesherif86@gmail.com>
2025-03-01 14:32:43 +0200

    feat: Implement Order repository, service, and controller

--- src/main/java/com/example/controller/OrderController.java
+++ src/main/java/com/example/controller/OrderController.java
@@ -1,4 +1,35 @@
 package com.example.controller;
 
+import com.example.model.Order;
+import com.example.service.OrderService;
+import com.example.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+    @PostMapping("/")
+    public void addOrder(@RequestBody Order order){
+        orderService.addOrder(order);
+    }
+    @GetMapping("/{orderId}")
+    public Order getOrderById(@PathVariable UUID orderId){
+        return orderService.getOrderById(orderId);
+    }
+    @GetMapping("/")
+    public ArrayList<Order> getOrders(){
+        return orderService.getOrders();
+    }
+    @DeleteMapping("/delete/{orderId}")
+    public String deleteOrderById(@PathVariable UUID orderId){
+        orderService.deleteOrderById(orderId);
+        return "Order deleted successfully";
+    }
 }
--- src/main/java/com/example/service/OrderService.java
+++ src/main/java/com/example/service/OrderService.java
@@ -1,4 +1,45 @@
 package com.example.service;
 
-public class OrderService {
+import com.example.model.Order;
+import com.example.repository.OrderRepository;
+import com.example.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@SuppressWarnings("rawtypes")
+public class OrderService  extends MainService<Order> {
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public void addOrder(Order order){
+        orderRepository.addOrder(order);
+    }
+    public ArrayList<Order> getOrders(){
+        return orderRepository.getOrders();
+    }
+    public Order getOrderById(UUID orderId){
+
+        Optional<Order> order = Optional.ofNullable(orderRepository.getOrderById(orderId));
+        if (order.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "order not found");
+        }
+        return orderRepository.getOrderById(orderId);
+    }
+
+    public void deleteOrderById(UUID orderId) throws IllegalArgumentException{
+
+        Optional<Order> order = Optional.ofNullable(orderRepository.getOrderById(orderId));
+        if (order.isEmpty()) {
+            throw new IllegalArgumentException("order not found");
+        }
+        orderRepository.deleteOrderById(orderId);
+    }
 }
+
